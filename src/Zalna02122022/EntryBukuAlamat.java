4b7fc40919651203f9c367051c9fc2eb83ebc76b
Zalna Mustika <109508971+zalnamustika11@users.noreply.github.com>
2022-12-02 12:04:02 +0700

    Create EntryBukuAlamat.java

---
+++ src/Zalna02122022/EntryBukuAlamat.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Zalna02122022;
+
+/**
+ *
+ * @author DELL
+ */
+public class EntryBukuAlamat {
+    public String nama;
+    public String alamat;
+    public String notelp;
+    public String email;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getNotelp() {
+        return notelp;
+    }
+
+    public void setNotelp(String notelp) {
+        this.notelp = notelp;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
